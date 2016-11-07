daa9f57002f0d8625b13904fa81204abc128e9c3
EvgeniiaMoi <ginny-mua@yandex.ru>
2016-11-07 15:27:24 +0200

    Styles chacges add

--- script.js
+++ script.js
@@ -8,4 +8,16 @@ $(document).ready(function(){
 	$('.events').find('.slick-prev').html('<i class="fa fa-2x fa-arrow-circle-o-left"></i>');
 	$('.events').find('.slick-next').html('<i class="fa fa-2x fa-arrow-circle-o-right"></i>');
 	$(".fancybox").fancybox();
+	$(function () {
+    	var tabContainers = $('div.pages > div');
+	
+    	$('div.arrowgreen ul.tabNavigation a').click(function () {
+    	    tabContainers.hide().filter(this.hash).show();
+	
+    	    $('div.arrowgreen ul.tabNavigation a').removeClass('selected');
+    	    $(this).addClass('selected');
+	
+    	    return false;
+    	}).filter(':first').click();
+	});
 });
